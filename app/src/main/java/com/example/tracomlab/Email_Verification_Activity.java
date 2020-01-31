b96e71d07273c6e139d7db979f233efcbbf99137
amwiti <developerbold@gmail.com>
2020-01-31 11:49:15 +0300

    OTP complete

--- app/src/main/java/com/example/tracomlab/Email_Verification_Activity.java
+++ app/src/main/java/com/example/tracomlab/Email_Verification_Activity.java
@@ -10,6 +10,17 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.tracomlab.R;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Email_Verification_Activity extends AppCompatActivity {
 
     Button button;
@@ -22,33 +33,38 @@ public class Email_Verification_Activity extends AppCompatActivity {
 
 
         button=findViewById(R.id.sendButton);
-       textInputLayout=findViewById(R.id.recoveryTextInput);
+        textInputLayout=findViewById(R.id.recoveryTextInput);
 
 
-       button.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-               String getEmail = textInputLayout.getEditText().getText().toString();
+                String getEmail = textInputLayout.getEditText().getText().toString();
 
-               if ( textInputLayout.getEditText().getText().toString() != null){
-
-                   Toast.makeText(Email_Verification_Activity.this,""+getEmail,Toast.LENGTH_LONG).show();
-
-                   Intent intent=new Intent(Email_Verification_Activity.this,OneTimePasswordActivity.class);
-                   startActivity(intent);
-                   finish();
-
-                   return;
-               }
-
-               Toast.makeText(Email_Verification_Activity.this,"Can't send Email on \n Null Email Address",Toast.LENGTH_LONG).show();
-               textInputLayout.getEditText().setError("Blank Email");
+                if ( textInputLayout.getEditText().getText().toString().trim().isEmpty()){
 
 
+                    textInputLayout.getEditText().setError("");
 
-           }
-       });
+                    Toast.makeText(Email_Verification_Activity.this,"Can't send Email on \n Null Email Address",Toast.LENGTH_LONG).show();
+                    textInputLayout.getEditText().setError("Blank Email");
+
+
+                    return;
+                }
+
+                Toast.makeText(Email_Verification_Activity.this,""+getEmail,Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(Email_Verification_Activity.this, OneTimePasswordActivity.class);
+                startActivity(intent);
+                finish();
+
+
+
+
+            }
+        });
 
 
     }
